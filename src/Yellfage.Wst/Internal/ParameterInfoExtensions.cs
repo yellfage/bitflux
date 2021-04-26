ef1191144eb75e2325bb3f2481da32c7cfa821e7
Randell <irandellhart@gmail.com>
2021-04-26 11:50:20 +0300

    feat: add ParameterInfoExtensions.IsFlexible()

--- src/Yellfage.Wst/Internal/ParameterInfoExtensions.cs
+++ src/Yellfage.Wst/Internal/ParameterInfoExtensions.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Reflection;
+
 namespace Yellfage.Wst.Internal
 {
     internal static class ParameterInfoExtensions
     {
+        public static bool IsFlexible(this ParameterInfo info)
+        {
+            return info.IsDefined(typeof(ParamArrayAttribute), false);
+        }
     }
 }
