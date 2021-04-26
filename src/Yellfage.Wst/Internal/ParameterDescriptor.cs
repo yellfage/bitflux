36ec7e96c95025df5a61b751aa74ff7c3049a959
Randell <irandellhart@gmail.com>
2021-04-26 17:27:35 +0300

    feat(internal): add ParameterDescriptor.IsFlexible

--- src/Yellfage.Wst/Internal/ParameterDescriptor.cs
+++ src/Yellfage.Wst/Internal/ParameterDescriptor.cs
@@ -5,10 +5,12 @@ namespace Yellfage.Wst.Internal
     internal class ParameterDescriptor
     {
         public ParameterInfo Info { get; }
+        public bool IsFlexible { get; }
 
         public ParameterDescriptor(ParameterInfo info)
         {
             Info = info;
+            IsFlexible = info.IsFlexible();
         }
     }
 }
