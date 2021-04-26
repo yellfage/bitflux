c1c0a768696a60c6821216d035b89b239684a318
Randell <irandellhart@gmail.com>
2021-04-26 16:51:02 +0300

    feat(internal): add IListExtensions.RemoveRange()

--- src/Yellfage.Wst/Internal/IListExtensions.cs
+++ src/Yellfage.Wst/Internal/IListExtensions.cs
@@ -1,6 +1,20 @@
+using System.Collections.Generic;
+
 namespace Yellfage.Wst.Internal
 {
     internal static class IListExtensions
     {
+        public static void RemoveRange<T>(this IList<T> list, int index)
+        {
+            RemoveRange(list, index, list.Count - index);
+        }
+
+        public static void RemoveRange<T>(this IList<T> list, int index, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                list.RemoveAt(index);
+            }
+        }
     }
 }
