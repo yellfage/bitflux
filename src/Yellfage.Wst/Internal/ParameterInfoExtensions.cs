e5771fc552f2be8bda44a2284f6fcf576dfe07e7
Randell <irandellhart@gmail.com>
2021-04-26 16:53:37 +0300

    feat(internal): add ParameterInfoExtensions.IsNullable()

--- src/Yellfage.Wst/Internal/ParameterInfoExtensions.cs
+++ src/Yellfage.Wst/Internal/ParameterInfoExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Reflection;
 
 namespace Yellfage.Wst.Internal
@@ -8,6 +9,28 @@ namespace Yellfage.Wst.Internal
         public static bool IsFlexible(this ParameterInfo info)
         {
             return info.IsDefined(typeof(ParamArrayAttribute), false);
+        }
+
+        public static bool IsNullable(this ParameterInfo info)
+        {
+            if (info.ParameterType.IsValueType)
+            {
+                return Nullable.GetUnderlyingType(info.ParameterType) is not null;
+            }
+
+            Attribute? attribute = info
+                .GetCustomAttributes()
+                .FirstOrDefault(attribute =>
+                    attribute.GetType().FullName == "System.Runtime.CompilerServices.NullableAttribute");
+
+            if (attribute is null)
+            {
+                return false;
+            }
+
+            byte[] flags = (byte[])attribute.GetType().GetField("NullableFlags")!.GetValue(attribute)!;
+
+            return flags[0] == 2;
         }
     }
 }
